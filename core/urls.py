48caa42c5c964c74018ce5d59dca49292b4b844a
Isi.aguilar@duocuc.cl <isi.aguilar@duocuc.cl>
2025-04-18 23:42:35 -0400

    añadir carpeta static, carpeta template y configuraciones

---
+++ core/urls.py
@@ -0,0 +1,30 @@
+from django.urls import path
+from .views import index
+from .views import accion
+from .views import aventura
+from .views import compra
+from .views import deportes
+from .views import estrategia
+from .views import formulario
+from .views import inicio_sesion
+from .views import modificacion_perfil
+from .views import recuperar_contraseña
+from .views import rol
+from .views import simulacion
+
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('accion/', accion, name='accion'),
+    path('aventura/', aventura, name='aventura'),
+    path('compra/', compra, name='compra'),
+    path('deportes/', deportes, name='deportes'),
+    path('estrategia/', estrategia, name='estrategia'),
+    path('formulario/', formulario, name='formulario'),
+    path('inicio-sesion/', inicio_sesion, name='inicio_sesion'),
+    path('modificacion-perfil/', modificacion_perfil, name='modificacion_perfil'),
+    path('formulario/', formulario, name='formulario'),
+    path('recuperar-contraseña/', recuperar_contraseña, name='recuperar_contraseña'),
+    path('rol/', rol, name='rol'),
+    path('simulacion/', simulacion, name='simulacion'),
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+     return render(request, 'index.html')
+
+def accion(request):
+     return render(request, 'accion.html')
+
+def aventura(request):
+     return render(request, 'aventura.html')
+
+def compra(request):
+     return render(request, 'compra.html')
+
+def deportes(request):
+     return render(request, 'deportes.html')
+
+def estrategia(request):
+     return render(request, 'estrategia.html')
+
+def formulario(request):
+     return render(request, 'formulario.html')
+
+def inicio_sesion(request):
+     return render(request, 'inicio_sesion.html')
+
+def modificacion_perfil(request):
+     return render(request, 'modificacion_perfil.html')
+
+def recuperar_contraseña(request):
+     return render(request, 'recuperar_contraseña.html')
+
+def rol(request):
+     return render(request, 'rol.html')
+
+def simulacion(request):
+     return render(request, 'simulacion.html')
