59a4ab56d1e3c32ad880ccf78fd048adf298b0c5
Isi.aguilar@duocuc.cl <isi.aguilar@duocuc.cl>
2025-04-18 23:43:10 -0400

    se agregan cambios de rutas

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from .views import index
 from .views import accion
 from .views import aventura
--- proyecto_pyme_juegos_django/settings.py
+++ proyecto_pyme_juegos_django/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,6 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'core.statics'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
