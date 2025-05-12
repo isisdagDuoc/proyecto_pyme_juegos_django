c3dc183461a1f2e488d52873545e6c458d2c7f44
Isis Aguilar <isisdanielaaguilar@gmail.com>
2025-05-11 20:43:10 -0400

    se agrega el cambio de rest framework

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,4 @@ urlpatterns = [
     path('login/', views.iniciar_sesion, name='iniciar_sesion'),
     path('modificar_perfil/', views.modificar_perfil, name='modificar_perfil'),
     path('eliminar_usuario/', views.eliminar_usuario, name='eliminar_usuario'),
-
-
-    
 ]
--- rest_api/views.py
+++ rest_api/views.py
@@ -25,29 +25,16 @@ def obtener_categoria(request):
     except requests.Timeout:
         return Response({"detail": "Timeout"}, status=504)
     
+@api_view(["GET"])
 def obtener_noticias(request):
     try:
-
-        response = requests.get('https://newsapi.org/v2/everything?q=videojuegos&language=es&apiKey=bf7610163c13409f960047b0d624d77d', timeout=5)
-        response.raise_for_status()
-        data = response.json()
-
-        return JsonResponse(data, safe=False, status=200)
-    except requests.exceptions.Timeout:
-        return JsonResponse(
-            {'error': 'Tiempo de espera agotado al llamar al servicio externo.'},
-            status=504
-        )
-    except requests.exceptions.HTTPError as e:
-        return JsonResponse(
-            {'error': f'Error HTTP {e.response.status_code} al llamar al servicio externo.'},
-            status=e.response.status_code
-        )
-    except requests.exceptions.RequestException as e:
-        return JsonResponse(
-            {'error': f'No se pudo obtener la información: {str(e)}'},
-            status=502
-        )
+        data = requests.get(
+            "https://newsapi.org/v2/everything?q=videojuegos&language=es&apiKey=bf7610163c13409f960047b0d624d77d",
+            timeout=5
+        ).json()
+        return Response(data, status=200)
+    except requests.Timeout:
+        return Response({"detail": "Timeout"}, status=504)
 
 def wiki(request):
     return render(request, 'wiki.html')
